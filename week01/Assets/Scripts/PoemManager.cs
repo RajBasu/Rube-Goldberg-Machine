43ffc7941754fc1539291d70cc16b6468e0d6543
Raj Basu <rb3478@nyu.edu>
2014-07-15 11:51:22 -0400

    Poetic Landscape - Raj Basu 2014

---
+++ week01/Assets/Scripts/PoemManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoemManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello World");
+		GetComponent<TextMesh> ().text ="Sting-chill of winter matt –\n her frozen hand caressed us all;\nAnd calming bleach of silence\npressed upon the rustic scape \nTo leave an ashen underbelly –\nOnce raging summer chroma.";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
